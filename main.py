824737989ffb62e2a86479354fbe03916adae21b
Nachiket Kapure <93987508+Nachiket1904@users.noreply.github.com>
2025-10-14 22:15:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,10 +19,12 @@ app.include_router(bots.router)
 app.include_router(chat.router)
 # app.include_router(files.router)
 
-# ✅ Serve static files (logos, PDFs, etc.)
-bots_dir = os.path.join(os.getcwd(), "bots")
-app.mount("/bots", StaticFiles(directory=bots_dir), name="bots")
+# # ✅ Serve static files (logos, PDFs, etc.)
+# bots_dir = os.path.join(os.getcwd(), "bots")
+# app.mount("/bots", StaticFiles(directory=bots_dir), name="bots")
+app.mount("/static", StaticFiles(directory="bots"), name="static")
 
 @app.get("/")
 def home():
     return {"message": "RAG Bot API is running 🚀"}
+
